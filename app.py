fd62ec85ef381f3a0b828c84574d1f6c16422304
VladiBan <vabanov24a@gmail.com>
2026-02-17 14:08:05 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+
+books = [
+    "the hobbit",
+    "Hari potnia",
+    "Mythology",
+    " 1984"
+
+]
+
+st.title("Vook checer app")
+st.write("enter book title to see if it exists in our data base.")
+user_input = st.text_input("book title")
+if st.button("check book"):
+    if user_input.strip() == "":
+        st.warning(" please enter book title")
+    elif user_input in books:
+        st.succsess(" the book exists in our data base ")
+    else:
+        st.error(" the book does NOT exist in our data base ")
